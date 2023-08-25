d94804c895ab60018a1c622e09c65c33833a077c
LightLai813 <813lightlai@gmail.com>
2023-08-25 13:58:30 +0800

    init

---
+++ .storybook/VponTheme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Vpon UI Library',
+  brandUrl: 'https://data.vpon.com',
+  brandImage: '/assets/vpon-logo.svg',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import VponTheme from './VponTheme';
+
+addons.setConfig({
+  theme: VponTheme,
+});
